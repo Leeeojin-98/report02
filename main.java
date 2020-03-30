6b7a3988547d56f038a41e5ce5bcfb5d96ec7ac9
Leeeojin-98 <62531004+Leeeojin-98@users.noreply.github.com>
2020-03-30 18:21:02 +0900

    이중연결리스트 DList.java

--- main.java
+++ main.java
@@ -1,38 +1,21 @@
-package gu;
-import java.util.Arrays;
-
+package DList;
 public class main {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Student[] a = {
-				new Student(14001,"권나라", "국어", 5), new Student(12001, "김다미", "디자인", 1),
-				new Student(14003,"김동희", "수학", 4), new Student(12002, "박서준", "호텔조리", 2),
-				new Student(14002,"이다윗", "법과정치", 3), new Student(12004, "손예진", "컴퓨터", 3),
-				new Student(14004,"현빈", "지리", 2), new Student(12005, "최예나", "컴퓨터", 4),
-				new Student(14005,"김채원", "수학", 1), new Student(12003, "조유리", "컴퓨터", 5)		
-		};
-		Arrays.sort(a);
-		print(a, "ID");
-		
-		Arrays.sort(a, Student.WITH_DEPT);
-		print(a, "학과");
-		
-		Arrays.sort(a, Student.WITH_NAME);
-		print(a, "이름");
-		
-		Arrays.sort(a, Student.WITH_GRADE);
-		print(a, "학년");	
-		
-	}
+	   public static void main(String[] args) {
+	      DList<String> s = new DList<String>();  // 이중 연결 리스트 객체 s 생성
+	      
+	        s.insertAfter(s.head,"apple"); 
+	        s.insertBefore(s.tail, "orange");
+	      s.insertBefore(s.tail,"cherry"); 
+	      s.insertAfter(s.head.getNext(),"pear"); 
+	      s.print(); System.out.println();
 
-	private static void print(Student[] a, String string) {
-		// TODO Auto-generated method stub
-	      System.out.println("===================================");
-	      System.out.println(string);
-	      for(Student s : a) {
-	         System.out.println(s.toString());
-		
+	      s.delete(s.tail.getPrevious());
+	      s.print(); System.out.println(); 
+	      
+	      s.insertBefore(s.tail,"grape");
+	      s.print(); System.out.println();
+	      s.delete(s.head.getNext()); s.print();      s.delete(s.head.getNext());s.print();
+	      s.delete(s.head.getNext()); s.print();      s.delete(s.head.getNext());s.print();
+	   }
 	}
-}
-}
-
+	 
